6e88522252d88bc6761b8bfe4ef8ea5730e6c07d
rjmcf <rjmcf@live.co.uk>
2020-06-17 11:57:14 +0100

    Add new validation, ensure all messages can be seen when alerts fail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let numTimes = {};
-let emailReportTextNode;
+let textPara;
 let submitButton;
+const dateTimeFormat = new Intl.DateTimeFormat('en', {month: 'short', day: '2-digit'});
 
 function createDropDown(id)
 {
@@ -116,18 +117,130 @@ function setAvailable(cellId)
 
 function submit()
 {
+	if (validateData())
+	{
+		processData();
+	}
+}
+
+function validateData()
+{
 	const nameInput = document.getElementById("nameInput");
 	const username = nameInput.value.trim();
 	if (username === "")
 	{
 		alert("Please enter your name!");
-		return;
+		setErrorText("Please enter your name!");
+		return false;
 	}
 
+	// To fix JS handling of mods of -ve numbers
+	let dayNum = (((firstDay.getDay() - 1)%7)+7)%7;
+	let weekNum = 0;
+	let results = [];
+
 	let isValid = true;
-	let invalidDayIndices = [];
-	let sameTimeIndices = [];
-	let timeOutOfOrderIndices = [];
+	let invalidDayIndices = new Set();
+	let sameTimeIndices = new Set();
+	let timeOutOfOrderIndices = new Set();
+	let overlappingTimeSlotIndices = new Set();
+
+	for (let i = 0; i < numDays; i++)
+	{
+		// Update counters
+		if (i>0)
+		{
+			dayNum = (dayNum+1)%7;
+		}
+		if (dayNum == 0 && i > 0)
+	  	{
+	  		weekNum++;
+	  	}
+
+		if (dayIndicesToSkip.includes(i))
+		{
+			continue;
+		}
+
+		const cellId = `Day${weekNum}${dayNum}`;
+		const checkBox = document.getElementById(`${cellId}Check`);
+		if (checkBox.checked == false)
+		{
+			continue;
+		}
+
+		orderedByStartTimes = [];
+		for (let timeIndex = 0; timeIndex < numTimes[cellId]; timeIndex++)
+		{
+			const fromSelector = document.getElementById(`${cellId}From${timeIndex}`);
+			const toSelector = document.getElementById(`${cellId}To${timeIndex}`);
+
+			if (fromSelector === null || toSelector === null)
+			{
+				continue;
+			}
+
+			// Check for errors in entry
+			if (fromSelector.value === "-" || toSelector.value === "-")
+			{
+				isValid = false;
+				invalidDayIndices.add(i);
+				continue;
+			}
+
+			if (fromSelector.value === toSelector.value)
+			{
+				isValid = false;
+				sameTimeIndices.add(i);
+				continue;
+			}
+
+			if (ltForTimes(toSelector.value, fromSelector.value))
+			{
+				isValid = false;
+				timeOutOfOrderIndices.add(i);
+				continue;
+			}
+
+			insertIntoSorted({from: fromSelector.value,to: toSelector.value}, orderedByStartTimes);
+		}
+
+		if (hasOverlaps(orderedByStartTimes))
+		{
+			isValid = false;
+			overlappingTimeSlotIndices.add(i);
+		}
+	}
+
+	if (isValid)
+	{
+		return true;
+	}
+
+	let errorText = "Things to fix:\n\n";
+	if (invalidDayIndices.size)
+	{
+		errorText += alertInvalidDays(invalidDayIndices) + "\n\n";
+	}
+	if (sameTimeIndices.size)
+	{
+		errorText += alertSameTimes(sameTimeIndices) + "\n\n";
+	}
+	if (timeOutOfOrderIndices.size)
+	{
+		errorText += alertTimesOutOfOrder(timeOutOfOrderIndices) + "\n\n";
+	}
+	if (overlappingTimeSlotIndices.size)
+	{
+		errorText += alertOverlappingTimeSlots(overlappingTimeSlotIndices);
+	}
+	errorText = errorText.replace(/\n/g, "<br>");
+	setErrorText(errorText);
+	return false;
+}
+
+function processData()
+{
 	// To fix JS handling of mods of -ve numbers
 	let dayNum = (((firstDay.getDay() - 1)%7)+7)%7;
 	let weekNum = 0;
@@ -172,76 +285,38 @@ function submit()
 				continue;
 			}
 
-			// Check for errors in entry
-			if (fromSelector.value === "-" || toSelector.value === "-")
-			{
-				isValid = false;
-				invalidDayIndices.push(i);
-				continue;
-			}
-
-			if (fromSelector.value === toSelector.value)
-			{
-				isValid = false;
-				sameTimeIndices.push(i);
-				continue;
-			}
-
-			if (fromSelector.value > toSelector.value && toSelector.value !== "00:00")
-			{
-				isValid = false;
-				timeOutOfOrderIndices.push(i);
-				continue;
-			}
-
 			// Other wise record results
-			result["times"].push({from:fromSelector.value, to:toSelector.value});
+			insertIntoSorted({from:fromSelector.value, to:toSelector.value}, result.times);
 		}
 
 		results.push(result);
 	}
 
-	// If no errors found, send email with results
-	if (isValid)
+	const nameInput = document.getElementById("nameInput");
+	const username = nameInput.value.trim();
+
+	const output =
 	{
-		const output =
-		{
-			name: username,
-			firstDay: firstDay,
-			availability: results
-		};
-		const outputString = JSON.stringify(output);
+		name: username,
+		firstDay: firstDay,
+		availability: results
+	};
+	const outputString = JSON.stringify(output);
 
-		if (printToConsole)
-		{
-			console.log(outputString);
-		}
-
-		if (!disableEmails)
-		{
-			sendEmail(username, outputString);
-		}
+	if (printToConsole)
+	{
+		console.log(outputString);
 	}
-	else
+
+	if (!disableEmails)
 	{
-		if (invalidDayIndices.length)
-		{
-			alertInvalidDays(invalidDayIndices);
-		}
-		if (sameTimeIndices.length)
-		{
-			alertSameTimes(sameTimeIndices);
-		}
-		if (timeOutOfOrderIndices.length)
-		{
-			alertTimesOutOfOrder(timeOutOfOrderIndices);
-		}
+		sendEmail(username, outputString);
 	}
 }
 
 function sendEmail(name, stringToSend)
 {
-	emailReportTextNode.nodeValue = "Sending...";
+	setErrorText("Sending...");
 	submitButton.disabled = true;
 
 	Email.send({
@@ -269,7 +344,7 @@ function reportEmailSuccess(name)
 {
 	alert(`Availability submitted successfully. Thanks ${name}!`);
 
-	emailReportTextNode.nodeValue = `Data sent successfully, thanks ${name}!`;
+	setErrorText(`Data sent successfully, thanks ${name}!`);
 	submitButton.disabled = false;
 }
 
@@ -277,7 +352,7 @@ function reportEmailFail(name, message)
 {
 	alert(`"${message}"\nHmmm, looks like there's been a problem! Let Robin know and send him a screenshot of this error!`);
 
-	emailReportTextNode.nodeValue = `Uh oh! Something went wrong! Please send Robin the following message: "${message}".`;
+	setErrorText(`Uh oh! Something went wrong! Please send Robin the following message: "${message}".`);
 	submitButton.disabled = false;
 }
 
@@ -287,6 +362,7 @@ function alertInvalidDays(invalidDayIndices)
 	string = addDaysToString(string, invalidDayIndices);
 	string += "Please either select times for these days or mark yourself unavailable";
 	alert(string);
+	return string;
 }
 
 function alertSameTimes(sameTimeIndices)
@@ -295,6 +371,7 @@ function alertSameTimes(sameTimeIndices)
 	string = addDaysToString(string, sameTimeIndices);
 	string += "Please select different times for these days";
 	alert(string);
+	return string;
 }
 
 function alertTimesOutOfOrder(timeOutOfOrderIndices)
@@ -303,25 +380,116 @@ function alertTimesOutOfOrder(timeOutOfOrderIndices)
 	string = addDaysToString(string, timeOutOfOrderIndices);
 	string += "Please make sure the end time is after the start time";
 	alert(string);
+	return string;
+}
+
+function alertOverlappingTimeSlots(overlappingTimeSlotIndices)
+{
+	let string = "The following days have overlapping time slots:\n";
+	string = addDaysToString(string, overlappingTimeSlotIndices);
+	string += "Please make sure that no time slots overlap";
+	alert(string);
+	return string;
 }
 
 function addDaysToString(string, dayIndices)
 {
-	const dateTimeFormat = new Intl.DateTimeFormat('en', {month: 'short', day: '2-digit'});
-	dayIndices.forEach((item, index) =>
+	dayIndices.forEach((item) =>
 	{
 		const badDay = addDays(firstDay, item);
 		const [{ value: month },,{ value: day }] = dateTimeFormat.formatToParts(badDay);
-		string += `    ${day} ${month}\n`;
+		string += `- ${day} ${month}\n`;
 	});
 	return string;
 }
 
+function ltForTimes(time1, time2)
+{
+	if (time1 === time2)
+	{
+		return false;
+	}
+
+	if (time1 === "00:00")
+	{
+		return false;
+	}
+
+	if (time2 === "00:00")
+	{
+		return true;
+	}
+
+	return time1 < time2;
+}
+
+function minTime(time1, time2)
+{
+	return ltForTimes(time1, time2) ? time1 : time2;
+}
+
+function maxTime(time1, time2)
+{
+	return ltForTimes(time1, time2) ? time2 : time1;
+}
+
+function insertIntoSorted(timeSlot, sorted)
+{
+	for (let i = 0; i < sorted.length; i++)
+	{
+		if (ltForTimes(timeSlot.from, sorted[i].from))
+		{
+			sorted.splice(i, 0, timeSlot);
+			return;
+		}
+	}
+
+	sorted.push(timeSlot);
+}
+
+function hasOverlaps(sortedTimeSlots)
+{
+	for (let i = 0; i < sortedTimeSlots.length; i++)
+	{
+		for (let j = i + 1; j < sortedTimeSlots.length; j++)
+		{
+			if (
+				ltForTimes(
+					maxTime(sortedTimeSlots[i].from, sortedTimeSlots[j].from),
+					minTime(sortedTimeSlots[i].to, sortedTimeSlots[j].to)
+				)
+			)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function setErrorText(text)
+{
+	textPara.innerHTML = "";
+
+	const t = text.split(/\s*<br ?\/?>\s*/i);
+	if(t[0].length > 0)
+	{
+		textPara.appendChild(document.createTextNode(t[0]));
+	}
+	for(let i = 1; i < t.length; i++)
+	{
+		textPara.appendChild(document.createElement('br'));
+		if(t[i].length > 0)
+		{
+	  		textPara.appendChild(document.createTextNode(t[i]));
+		}
+	}
+}
+
 window.onload = function()
 {
-	emailReportTextNode = document.createTextNode("");
-	const textPara = document.getElementById("emailReport");
-	textPara.appendChild(emailReportTextNode);
+	textPara = document.getElementById("emailReport");
 
 	submitButton = document.getElementById("submitButton");
 
@@ -346,7 +514,6 @@ window.onload = function()
 
 	  	const id = "Day" + week + dayNum;
 
-        const dateTimeFormat = new Intl.DateTimeFormat('en', {month: 'short', day: '2-digit'});
 		const [{ value: month },,{ value: day }] = dateTimeFormat.formatToParts(currentDay);
 
 		const para = document.createElement("p");
